9225541cfdc329197404b8875c97442f111e7fa4
shreyaskar-1 <shreyaskar21@gmail.com>
2024-09-11 16:10:52 +0530

    Starter Pages With Navbar

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HeroSection from "./components/HeroSection";
+
+const About = () => {
+  const data = {
+    name: "Web Ecommerce",
+  };
+
+  return <HeroSection myData={data} />;
+};
+
+export default About;
